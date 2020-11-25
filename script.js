4770dc3f2c65a9f4cdf3b897aa4f0fb495045277
Ogwuru Patrick <sirpatrickchigozie@gmail.com>
2020-11-25 21:57:06 +0100

    Fixed function to toggle between opening and showing scrollable section.

--- script.js
+++ script.js
@@ -1,33 +1,18 @@
-/* JavaScript to toggle between showing and hiding accordion panel and assigning background color to the accordion buttons:*/
-var acc = document.getElementsByClassName("drop-btn");
-var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].onclick = function () {
-    this.classList.toggle("active");
-    this.nextElementSibling.classList.toggle("show");
-  };
-}
 /* JavaScript to toggle between showing and hiding the scrollable notes section*/
-/*let hideFunction = () => {
-  let toggle = document.getElementById("toggle");
-  let content = document.getElementById("content");
-  toggle.addEventListener("click", function () {
-    content.classList.toggle("show");
-  });
-  toggle2.addEventListener("click", function () {
-    content2.classList.toggle("show");
-  });
-};*/
+
 let hideFunction = () => {
   let toggle = document.getElementById("toggle");
   let content = document.getElementById("content");
+  let toggle2 = document.getElementById("toggle2");
+  let content2 = document.getElementById("content2");
   if (toggle) {
-    toggle.addEventListener("click", function () {
+    toggle.addEventListener("click", () => {
         content.classList.toggle("show");
     });
-  }else if(toggle2){
-    toggle2.addEventListener("click", function () {
+  }
+  if(toggle2){
+    toggle2.addEventListener("click", () => {
         content2.classList.toggle("show");
     });
   }
