1e7775c96a5d3faf7186a4f4b3668ee7bd44f62b
Ogwuru Patrick <sirpatrickchigozie@gmail.com>
2020-12-19 03:53:52 +0100

    Made basic styling to pages

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-
 /* JavaScript to toggle between showing and hiding the scrollable notes section*/
 let hideFunction = () => {
   let toggle = document.getElementById("toggle");
   let content = document.getElementById("content");
   let toggle2 = document.getElementById("toggle2");
   let content2 = document.getElementById("content2");
+
   if (toggle) {
     toggle.addEventListener("click", () => {
-        content.classList.toggle("show");
+      content.classList.toggle("show");
     });
   }
-  if(toggle2){
+  if (toggle2) {
     toggle2.addEventListener("click", () => {
-        content2.classList.toggle("show");
+      content2.classList.toggle("show");
     });
   }
-}
+};
 /* Javascript function to switch between show and hide for the dropdown*/
 let dropHome = () => {
-    let toggle = document.getElementById("home-btn");
-    let content = document.getElementById("drop-content");
+  let toggle = document.getElementById("home-btn");
+  let content = document.getElementById("drop-content");
 
-    toggle.addEventListener("click", () => {
-        content.classList.toggle("hide");
-    });
-}
+  toggle.addEventListener("click", () => {
+    content.classList.toggle("hide");
+  });
+};
