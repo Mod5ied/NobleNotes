778a2e9808ec527ae5fec8161bb24592ed9a530d
Ogwuru Patrick <sirpatrickchigozie@gmail.com>
2021-01-19 15:23:55 +0100

    Removed the vue instances and components

--- script.js
+++ script.js
@@ -25,28 +25,3 @@ let dropHome = () => {
     content.classList.toggle("hide");
   });
 };
-
-
-//Toggle "show/hide" component
-Vue.component("toggler", {
-  methods: {
-    
-  }
-});
-//Vue instance one
-const App1 = new Vue({
-  el: "#Vueapp1",
-  methods: {},
-});
-
-//Vue instance two
-const App1 = new Vue({
-  el: "#Vueapp1",
-  methods: {},
-});
-
-//Vue instance three
-const App1 = new Vue({
-  el: "#Vueapp1",
-  methods: {},
-});
